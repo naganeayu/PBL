1355038d12caadcb40c200ab9a3fee8640dfdf7b
naganeayu <naganeayu@me.com>
2021-04-25 13:29:16 +0900

    Update main.r

--- main.r
+++ main.r
@@ -12,13 +12,13 @@ freq <- RMeCabFreq(src)
 #品詞
 hinshi <- unique(freq$Info1)
 
+#not.inを利用して、不要な品詞を指定する
 "%not.in%" <- Negate("%in%")
 freq2 <- subset(freq, Info1 %not.in% c("フィラー", "助詞", "感動詞","接続詞","記号", "接頭詞", "副詞", "助動詞"))
 freq3 <- subset(freq2, Info2 %not.in% c("固有名詞","非自立","代名詞","接尾","数"))
 
 #不要な文字の削除
 freq4 <- subset(freq3, Term %not.in% c("する","ある","その","なる"))
-
 
 #ソート
 sorted <- freq4[order(freq4$Freq, decreasing = T),]
